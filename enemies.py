7e1cf8f644610a4ebb696e8e46c26de62416b5f4
Gabriel Matos dos Santos <gplaylgmg775@gmail.com>
2023-11-15 15:13:01 -0300

    Adicione animação de todos inimigos

--- enemies.py
+++ enemies.py
@@ -15,32 +15,21 @@ class Enemy(pygame.sprite.Sprite):
             self.kill()
 
 class EnemyType1(Enemy):
-    sprites = [
-            pygame.image.load("fairies/fairy4_0.png"), 
-            pygame.image.load("fairies/fairy4_1.png"), 
-            pygame.image.load("fairies/fairy4_2.png"), 
-            pygame.image.load("fairies/fairy4_3.png")
-            ]
 
     ANIMATION_COOLDOWN = 70
 
-    def __init__(self, screen_width, screen_height):
+    def __init__(self, screen_width, screen_height, default_image):
         super().__init__(screen_width, screen_height)
-        self.image = EnemyType1.sprites[0]
+        self.image = default_image
         self._animation_count = 0
         self._last_frame_time = 0 
         self._animation_cooldown = EnemyType1.ANIMATION_COOLDOWN
         self.rect = self.image.get_rect()
+        self.rect.bottom = 0
         self.rect.x = random.randint(20, screen_width - 20)
         self._x_speed = random.uniform(-0.8, 0.8)
 
-    def update(self):
-        super().update()
-
-        EnemyType1.update_animation(self)
-        EnemyType1.moviment(self)
-
-    def update_animation(self):
+    def update_animation(self, sprites):
         current_time = pygame.time.get_ticks()
 
         if current_time - self._last_frame_time > self._animation_cooldown:
@@ -51,7 +40,7 @@ class EnemyType1(Enemy):
             if self._animation_count >= len(self.sprites):
                 self._animation_count = 0
 
-            self.image = self.sprites[self._animation_count]
+            self.image = sprites[self._animation_count]
 
     def moviment(self):
         self.rect.x += self._x_speed
@@ -60,41 +49,23 @@ class EnemyType1(Enemy):
             self._x_speed *= -1
 
 class EnemyType2(Enemy):
-    sprites = [
-            pygame.image.load("fairies/fairy1_0.png"), 
-            pygame.image.load("fairies/fairy1_1.png"), 
-            pygame.image.load("fairies/fairy1_2.png"), 
-            pygame.image.load("fairies/fairy1_3.png")
-            ]
-
-    sprites_turn =  [
-            pygame.image.load("fairies/turn1_0.png"), 
-            pygame.image.load("fairies/turn1_1.png"), 
-            pygame.image.load("fairies/turn1_2.png"), 
-            pygame.image.load("fairies/turn1_3.png")
-            ]
 
     ANIMATION_COOLDOWN = 70
 
-    def __init__(self, screen_width, screen_height):
+    def __init__(self, screen_width, screen_height, default_image):
         super().__init__(screen_width, screen_height)
-        self.image = EnemyType2.sprites[0]
+        self.image = default_image
         self._animation_count = 0
         self._last_frame_time = 0 
         self._animation_cooldown = EnemyType2.ANIMATION_COOLDOWN
         self.rect = self.image.get_rect()
+        self.rect.bottom = 0
         self.rect.x = random.randint(20, screen_width - 20)
         self._speed = random.uniform(1.7, 1.8)
         self._x_speed = random.uniform(-2, 2)
         self._stay_y = random.randint(100, 300)
 
-    def update(self):
-        super().update()
-
-        EnemyType2.update_animation(self)
-        EnemyType2.moviment(self)
-
-    def update_animation(self):
+    def update_animation(self, sprites, sprites_turn):
         current_time = pygame.time.get_ticks()
 
         if self._x_speed == 0:
@@ -106,7 +77,7 @@ class EnemyType2(Enemy):
                 if self._animation_count >= len(self.sprites):
                     self._animation_count = 0
 
-                self.image = self.sprites[self._animation_count]
+                self.image = sprites[self._animation_count]
 
         elif self._x_speed < 0:
 
@@ -117,7 +88,7 @@ class EnemyType2(Enemy):
                 if self._animation_count >= len(self.sprites_turn):
                     self._animation_count = 0
 
-                flippled_turn = pygame.transform.flip(self.sprites_turn[self._animation_count], True, False)
+                flippled_turn = pygame.transform.flip(sprites_turn[self._animation_count], True, False)
 
                 self.image = flippled_turn
 
@@ -141,3 +112,100 @@ class EnemyType2(Enemy):
 
             if self.rect.left <= 0 or self.rect.right >= 800:
                 self._x_speed *= -1
+
+
+class FairyType1(EnemyType2):
+    sprites = [
+            pygame.image.load("fairies/fairy1_0.png"), 
+            pygame.image.load("fairies/fairy1_1.png"), 
+            pygame.image.load("fairies/fairy1_2.png"), 
+            pygame.image.load("fairies/fairy1_3.png")
+            ]
+
+    sprites_turn =  [
+            pygame.image.load("fairies/turn1_0.png"), 
+            pygame.image.load("fairies/turn1_1.png"), 
+            pygame.image.load("fairies/turn1_2.png"), 
+            pygame.image.load("fairies/turn1_3.png")
+            ]
+    def __init__(self, screen_width, screen_height):
+        super().__init__(screen_width, screen_height, self.sprites[0])
+    
+    def update(self):
+        super().update()
+        self.update_animation(self.sprites, self.sprites_turn)
+        self.moviment()
+
+class FairyType2(EnemyType2):
+    sprites = [
+            pygame.image.load("fairies/fairy2_0.png"), 
+            pygame.image.load("fairies/fairy2_1.png"), 
+            pygame.image.load("fairies/fairy2_2.png"), 
+            pygame.image.load("fairies/fairy2_3.png")
+            ]
+
+    sprites_turn =  [
+            pygame.image.load("fairies/turn2_0.png"), 
+            pygame.image.load("fairies/turn2_1.png"), 
+            pygame.image.load("fairies/turn2_2.png"), 
+            pygame.image.load("fairies/turn2_3.png")
+            ]
+    def __init__(self, screen_width, screen_height):
+        super().__init__(screen_width, screen_height, self.sprites[0])
+    
+    def update(self):
+        super().update()
+        self.update_animation(self.sprites, self.sprites_turn)
+        self.moviment()
+
+class FairyType3(EnemyType2):
+    sprites = [
+            pygame.image.load("fairies/fairy3_0.png"), 
+            pygame.image.load("fairies/fairy3_1.png"), 
+            pygame.image.load("fairies/fairy3_2.png"), 
+            pygame.image.load("fairies/fairy3_3.png")
+            ]
+
+    sprites_turn =  [
+            pygame.image.load("fairies/turn3_0.png"), 
+            pygame.image.load("fairies/turn3_1.png"), 
+            pygame.image.load("fairies/turn3_2.png"), 
+            pygame.image.load("fairies/turn3_3.png")
+            ]
+    def __init__(self, screen_width, screen_height):
+        super().__init__(screen_width, screen_height, self.sprites[0])
+    
+    def update(self):
+        super().update()
+        self.update_animation(self.sprites, self.sprites_turn)
+        self.moviment()
+
+class FairyType4(EnemyType1):
+    sprites = [
+            pygame.image.load("fairies/fairy4_0.png"), 
+            pygame.image.load("fairies/fairy4_1.png"), 
+            pygame.image.load("fairies/fairy4_2.png"), 
+            pygame.image.load("fairies/fairy4_3.png")
+            ]
+    def __init__(self, screen_width, screen_height):
+        super().__init__(screen_width, screen_height, self.sprites[0])
+
+    def update(self):
+        super().update()
+        self.update_animation(self.sprites)
+        self.moviment()
+
+class FairyType5(EnemyType1):
+    sprites = [
+            pygame.image.load("fairies/fairy5_0.png"), 
+            pygame.image.load("fairies/fairy5_1.png"), 
+            pygame.image.load("fairies/fairy5_2.png"), 
+            pygame.image.load("fairies/fairy5_3.png")
+            ]
+    def __init__(self, screen_width, screen_height):
+        super().__init__(screen_width, screen_height, self.sprites[0])
+
+    def update(self):
+        super().update()
+        self.update_animation(self.sprites)
+        self.moviment()
