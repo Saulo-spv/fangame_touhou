eaab98584fa422445b6c7f23008f82c9859fb082
Saulo Pires <saulopiresvieira5@gmail.com>
2023-11-15 15:02:44 -0300

    Adicione tiros para o player

--- game.py
+++ game.py
@@ -3,36 +3,38 @@ from player import Player
 
 class Game:
     def __init__(self, screen):
+        # Inicializa o jogo
         self.screen = screen
         self.clock = pygame.time.Clock()
 
-        # Carregando o sprite sheet de Touhou
+        # Carrega o sprite sheet de Touhou
         sprite_sheet = pygame.image.load("images/sprite_sheet.png")
 
-        # Criação de sprites
-        player_sprite_coordinates = (0, 0, 32, 50)
-        sprite_width = 32
-        sprite_height = 46
-        screen_height = screen.get_height()
-        screen_width = screen.get_width()
-
-        self.player = Player(sprite_sheet, *player_sprite_coordinates, sprite_width, sprite_height, screen_height, screen_width)
+        # Grupos de sprites para todos os sprites e balas
         self.all_sprites = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
+
+        # Cria o jogador e adiciona ao grupo de todos os sprites
+        self.player = Player(sprite_sheet, self.all_sprites, self.bullets)
         self.all_sprites.add(self.player)
 
     def run(self):
+        # Loop principal do jogo
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            keys = pygame.key.get_pressed()
-            self.all_sprites.update(keys)
+            # Atualiza os sprites
+            self.all_sprites.update()
+            self.bullets.update()
 
-            # Desenha os sprites
+            # Desenha os sprites na tela
             self.screen.fill((0, 0, 0))
             self.all_sprites.draw(self.screen)
+            self.bullets.draw(self.screen)
 
+            # Atualiza a tela
             pygame.display.flip()
             self.clock.tick(60)
--- player.py
+++ player.py
@@ -1,18 +1,18 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, sprite_sheet, x, y, width, height, sprite_width, sprite_height, screen_height, screen_width):
+    def __init__(self, sprite_sheet, all_sprites, bullets):
         super().__init__()
 
         # Configurações iniciais do jogador
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.sprite_width = sprite_width
-        self.sprite_height = sprite_height
-        self.screen_height = screen_height
-        self.screen_width = screen_width
+        self.x = 0
+        self.y = 0
+        self.width = 32
+        self.height = 50
+        self.sprite_width = 32
+        self.sprite_height = 46
+        self.screen_height = pygame.display.get_surface().get_height()
+        self.screen_width = pygame.display.get_surface().get_width()
         self.current_line = 0
         self.current_frame = 0
         self.sprite_sheet = sprite_sheet
@@ -20,12 +20,15 @@ class Player(pygame.sprite.Sprite):
         # Configura a imagem inicial do jogador
         self.image = self.sprite_sheet.subsurface(pygame.Rect(self.x, self.y, self.sprite_width, self.sprite_height)).convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.center = (width // 2, self.screen_height - 50)
+        self.rect.center = (self.screen_width // 2, self.screen_height - 50)
 
         self.speed = 5
+        self.all_sprites = all_sprites
+        self.bullets = bullets
 
-    def update(self, keys):
+    def update(self):
         # Configura a movimentação
+        keys = pygame.key.get_pressed()
         if keys[pygame.K_UP] and keys[pygame.K_LEFT]:
             self.current_line = 1
             self.rect.y -= self.speed / 3**0.5
@@ -57,6 +60,10 @@ class Player(pygame.sprite.Sprite):
         else:
             self.current_line = 0
 
+        # Dispara o tiro
+        if keys[pygame.K_SPACE]:
+            self.fire_bullet()
+
         self.screen_limit()
 
         self.update_animation()
@@ -66,16 +73,43 @@ class Player(pygame.sprite.Sprite):
         self.rect.x = max(-10, min(self.rect.x, self.screen_width - self.sprite_width * 1.5))
         self.rect.y = max(-5, min(self.rect.y, self.screen_height - self.sprite_height * 1.2))
 
-    def update_animation(self):
-        # Velocidade da animação
-        animation_speed = 5
-        self.current_frame = (self.current_frame + 1) % (8 * animation_speed)
+    def fire_bullet(self):
+        # Cria um novo tiro e o adiciona ao grupo de sprites
+        bullet = Bullet(self.rect.centerx, self.rect.top, self.sprite_sheet)
+        self.all_sprites.add(bullet)
+        self.bullets.add(bullet)
 
-        # Ajuste a velocidade da animação
+    def update_animation(self):
+        # Ajusta a velocidade da animação
+        animation_speed = 7
+        self.current_frame = (self.current_frame + 1) % (8 * animation_speed)
         self.y = self.current_line * self.sprite_height
         self.x = (self.current_frame // animation_speed) * self.sprite_width
 
         # Atualiza a imagem do jogador com a parte correta do sprite sheet
         self.image = self.sprite_sheet.subsurface(pygame.Rect(self.x, self.y, self.sprite_width, self.sprite_height)).convert_alpha()
 
-        self.image = pygame.transform.scale(self.image, (60,60))
+        self.image = pygame.transform.scale(self.image, (60, 60))
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, sprite_sheet):
+        super().__init__()
+
+        # Posição do tiro no sprite sheet
+        bullet_x = 0
+        bullet_y = 150
+        bullet_width = 16
+        bullet_height = 16 
+
+        # Inicializa e posiciona o tiro
+        self.image = sprite_sheet.subsurface(pygame.Rect(bullet_x, bullet_y, bullet_width, bullet_height)).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (x, y)
+        self.speed = 10
+
+    def update(self):
+        self.rect.y -= self.speed
+
+        # Remove o tiro quando atinge o topo da tela
+        if self.rect.bottom < 0:
+            self.kill()
