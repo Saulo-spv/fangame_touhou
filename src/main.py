d0cb5d972eed24657660102759acf970788eed2b
Yure Santos de Oliveira <yure.sdo@gmail.com>
2023-11-16 13:57:56 -0300

    atualize main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+
+from classes.game import Game
+
+if __name__ == '__main__':
+    pygame.init()
+
+    game = Game()
+    game.run()
+
+    pygame.quit()
+    sys.exit()
