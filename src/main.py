c5f8d560adbc3953f10da09d6f292beb425eb4ea
Saulo Pires <saulopiresvieira5@gmail.com>
2023-12-06 15:51:56 -0300

    Adicione classe menu.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import pygame
 import sys
-
 from classes.game import Game
-
+from classes.menu import Menu
 
 if __name__ == '__main__':
     pygame.init()
+
+    menu = Menu()
+    menu.run_menu()
+
 
     game = Game()
     game.run()
