02c1ce5a06743ae5daa46880afce79eb862e8ac2
Yure Santos de Oliveira <yure.sdo@gmail.com>
2023-12-07 02:37:52 -0300

    adicione interface de vida e pontuação

--- src/classes/game.py
+++ src/classes/game.py
@@ -1,5 +1,6 @@
 import pygame
 
+from classes.menu import Menu
 from classes.player import Player
 from classes.background import Background
 from classes.enemies import *
@@ -7,14 +8,16 @@ from classes.spawner import Spawn
 
 class Game:
     def __init__(self):
-        # Inicializa o jogo
         self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
 
-        # Cria o background e adiciona ao grupo de todos os sprites
         self.background = Background((800, 600), -7768, 1, 'assets/images/background/starfield.png')
 
-        # Cria o jogador e adiciona ao grupo de todos os sprites
+        self.life_heart = pygame.image.load('assets/images/interface/heart.png').subsurface(0, 0, 16, 16).convert_alpha()
+        self.life_heart = pygame.transform.scale(self.life_heart, (30, 30))
+
+        self.font = pygame.font.Font('assets/fonts/Silkscreen-Regular.ttf', 20)
+
         self.player = Player()
 
         self.all_enemies = pygame.sprite.Group()
@@ -23,6 +26,32 @@ class Game:
 
         self.player_bullets = pygame.sprite.Group()
         self.enemy_bullets = pygame.sprite.Group()
+
+        try:
+            data = open('highscore.txt', 'r')
+            self.__highscore = int(data.read())
+            data.close()
+        except IOError and ValueError:
+            self.__highscore = 0
+
+        self.__score = 0
+        self.__highscore
+    
+    @property
+    def score(self):
+        return self.__score
+    
+    @score.setter
+    def score(self, value):
+        self.__score = value
+    
+    @property
+    def highscore(self):
+        return self.__highscore
+    
+    @highscore.setter
+    def highscore(self, value):
+        self.__highscore = value
     
     def handle_collision(self):
         for bullet in self.player_bullets:
@@ -35,7 +64,31 @@ class Game:
             if self.player.hitbox.colliderect(bullet.hitbox):
                 self.player.get_hit()
                 bullet.kill()
+    
+    def update_interface(self):
+        heart_x_pos = 40
+        for i in range(self.player.life):
+            self.screen.blit(self.life_heart, (10 + heart_x_pos, 20))
+            heart_x_pos += 40
+        
+        score_label = self.font.render(f'SCORE: {self.score}', 1, (255, 0, 0))
+        self.screen.blit(score_label, (550,15))
 
+        highscore_label = self.font.render(f'HIGHSCORE: {self.highscore}', 1, (255, 0, 0))
+        self.screen.blit(highscore_label, (550,40))
+    
+    def update_score(self):
+        self.score += 2
+
+        if self.score >= self.highscore:
+            self.highscore = self.score
+    
+    def update_highscore(self):
+        if self.highscore == self.score:
+            data = open('highscore.txt', 'w')
+            data.write(str(self.highscore))
+            data.close()
+        
     def run(self):
         # Loop principal do jogo
         running = True
@@ -68,8 +121,13 @@ class Game:
 
             self.handle_collision()
 
+            self.update_score()
+
+            self.update_interface()
+
             if self.player.life <= 0:
                 running = False
+                self.update_highscore()
 
             # Atualiza a tela
             pygame.display.flip()
